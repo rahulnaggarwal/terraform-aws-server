67fb32badba2dcf754edd42f89ebfb62077f65f7
rahulnaggarwal <61568217+rahulnaggarwal@users.noreply.github.com>
2020-05-14 15:51:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,24 +28,3 @@ output "public_ip" {
 output "public_dns" {
   value = aws_instance.web.*.public_dns
 }
-Commit the changes into GitHub.
-
-Update github repository
-Back in your ptfe-workspace repository created earlier.
-
-Add a new folder called app-web-modules/.
-
-Create a single main.tf file with the following contents:
-
-provider "aws" {
-}
-
-module "server" {
-  source = "gov.tfe.rocks/YOUR_ORG_NAME/server/aws"
-
-  ami                    = var.ami
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-  identity               = var.identity
-  web_count              = var.web_count
-}
